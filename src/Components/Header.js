55428c9761c3389b3e33d83771a21578900a4fbe
Vinay8431 <vk826062@gmail.com>
2023-11-17 12:15:56 +0530

    third

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div>
+        <p className='para'>
+        <center>Header</center>
+        </p>
+    </div>
+  )
+}
+
+export default Header
